64c5c02e7ff6f75ab739323e1ae3eca7ad2a26e3
Alesya Osipenko <alesya-osipenko@mail.ru>
2021-12-14 22:47:42 +0300

    add slick-slider

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.replies__slider').slick({
+        arrows: true,
+        dots: false,
+        adaptiveHeight: true,
+        speed: 1000,
+        easing: 'ease',
+        infinite: true,
+        autoplaySpeed: 4000,
+        touchMove: true
+    });
+});
